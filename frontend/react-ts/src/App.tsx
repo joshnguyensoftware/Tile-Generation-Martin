bff1e0f7e8144fa72b2c0690506a1afdd88326b3
Josh Eroad <josh.nguyen@eroad.com>
2026-02-12 11:39:05 +1300

    Update TOTAL_FEATURES constant and modify header in App component

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -3,7 +3,7 @@ import Map from './components/map';
 function App() {
   return (
     <div className="App">
-      <h1>My Martin Map</h1>
+      <h1>Martin (Lazy Loading) Map</h1>
       <div style={{ height: '80vh', width: '100%' }}>
         <Map/>
       </div>
